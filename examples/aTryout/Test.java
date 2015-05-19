9b9f6144535f047924963e34d36091901e891b12
s0210435 <jochen.tombal@student.kuleuven.be>
2015-05-20 00:44:17 +0200

    werkende selectie maar zelfde probleem dat die 3x wordt opgeroepen

--- examples/aTryout/Test.java
+++ examples/aTryout/Test.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -82,11 +83,11 @@ public class Test extends PApplet{
 	    ListSelectionModel rowSelection = table.getSelectionModel();
 	    rowSelection.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	    
-	    JFrame f = new JFrame();
-	    f.setTitle(title);
-	    f.setSize(300, 300);
-	    f.add(new JScrollPane(table));
-	    f.setVisible(true);
+	    JDialog fAskStation = new JDialog();
+	    fAskStation.setTitle(title);
+	    fAskStation.setSize(300, 300);
+	    fAskStation.add(new JScrollPane(table));
+	    fAskStation.setVisible(true);
 	    
 	    
 
@@ -115,7 +116,7 @@ public class Test extends PApplet{
 			//do nothing
 		}
 		
-		f.dispose();
+		fAskStation.dispose();
 		
 		
 		return result[0];
@@ -190,7 +191,7 @@ public class Test extends PApplet{
 		this.allTrainsForDay = this.getTrainsForDay(20131215);
 		initMarker();
 		//Test jochen
-		showDepartingTrainsInStation((float) 32335,"18:04:00");
+			showDepartingTrainsInStation((float) 32335,"18:04:00");
 
 	}
 	
@@ -327,7 +328,7 @@ public class Test extends PApplet{
 	    Vector columnNamesV = new Vector(Arrays.asList(columnNames));
 	    final JTable table = new JTable(rowData, columnNamesV);
 	    
-	    JFrame f = new JFrame();
+	    JDialog f = new JDialog();
 	    f.setSize(300, 300);
 	    f.add(new JScrollPane(table));
 	    f.setVisible(true);
@@ -508,11 +509,16 @@ public class Test extends PApplet{
 	// set van alle trip_id (geen dubbelen) vanaf bepaalde tijd (zoeken in lijst van alle trains vanaf index)
 	// trip_id via stop_times vertrektijd en lijst met alle stopplaatsen + tijd
 	
+
 	
 	//show all stops in table (using JTable) 
 			private void showDepartingTrainsInStation(Float stop_id, String timenow){
+				final String[] resultingSelection = {null};
+				resultingSelection[0]=null;
+
 				try{
 					List<DataStopTime> departingTrains = getStopTimesFromStation(stop_id, timenow);
+					
 					
 					String stopName = this.stops.get(stop_id).stop_name;
 					
@@ -526,19 +532,19 @@ public class Test extends PApplet{
 								break;
 							}
 						}
-					      Vector colData = new Vector( Arrays.asList(stop.arrival_time, stop.departure_time, tripname));
+					      Vector colData = new Vector( Arrays.asList(stop.arrival_time, stop.departure_time, tripname, stop.trip_id));
 					      //Vector colData = new Vector( Arrays.asList(stop.trip_id, this.stops.get(stop.stop_id).stop_name,stop.arrival_time,stop.departure_time,stop.stop_sequence.toString()));
 					      rowData.add(colData);
 				    }
 					
-				
+				System.out.println("Setting up departingTable");
 				//Setup table
-			    String[] columnNames = {"Arrival Time", "Departure Time", "Name route"};
+			    String[] columnNames = {"Arrival Time", "Departure Time", "Name route", "trip ID"};
 			    
 			    Vector columnNamesV = new Vector(Arrays.asList(columnNames));
-			    JTable table = new JTable(rowData, columnNamesV);
+			    final JTable table = new JTable(rowData, columnNamesV);
 			    
-			    JFrame fdeparting = new JFrame();
+			    JDialog fdeparting = new JDialog();
 			    fdeparting.setTitle("Current Station: "+ stopName);
 			    //f.setTitle("Departing Trains from " + this.stops.get(stop_id) );
 			    fdeparting.setSize(600,600);
@@ -546,9 +552,111 @@ public class Test extends PApplet{
 			    fdeparting.setVisible(true);
 			    
 			    
+			    table.setRowSelectionAllowed(true);
+				System.out.println("done departingtapble");
+
+
+			    ListSelectionModel rowSelection = table.getSelectionModel();
+			    rowSelection.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);	
+			    
+			    System.out.println("selected mode");
+					
+			    rowSelection.addListSelectionListener(new ListSelectionListener() {
+			        public void valueChanged(ListSelectionEvent e) {	          
+			          int row = table.getSelectedRow();
+
+			          String trip_id = (String) table.getModel().getValueAt(row, 3);
+			          System.out.println("Selected: " + trip_id);
+			          resultingSelection[0] = trip_id;
+			          System.out.println("Selected array: " + resultingSelection[0]);
+
+			        }
+
+			      });
+			   
+			    System.out.println("going in while loop");
+		          System.out.println("After Selected array: " + resultingSelection[0]);
+		          String nullStr = null;
+		          try {
+					Thread.sleep(500);
+					while(resultingSelection[0].equals(nullStr))
+							Thread.sleep(500);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+			    System.out.println("coming out while loop");
+
+				
+				fdeparting.dispose();			    
+			    
+			    String trip_id = resultingSelection[0];
+			    
+			   List< DataStopTime> trainList = this.stoptimes.get(trip_id);
+			   DataStopTime train = null;
+			   for(DataStopTime i : trainList){
+					if(i.stop_id.equals(stop_id)){
+						train = i;
+						break;
+					}
+				}
+
+			    		
+			    
+			   List<DataStopTime> listNextTrains = getStopTimesFromDeparingTrain(train);
+			    showRestingTrainsInRoute(listNextTrains, departingRouteName);
+			   			    
+			    
 				}catch (ParseException e){
 					e.printStackTrace();
 				}
+			}
+			
+			private Float chosenDepartingTrain;
+			private void setChosenDepartingTrain(Float chosenDepartingTrain){
+				this.chosenDepartingTrain = chosenDepartingTrain;
+			}
+			private Float getChosenDepartingTrain(){
+				return chosenDepartingTrain;
+			}
+			private String departingRouteName;
+			private void setDepartingRouteName(String departingRouteName){
+				this.departingRouteName = departingRouteName;
+			}
+			private String getDepartingRouteName(){
+				return departingRouteName;
+			}
+			
+			
+			
+
+			
+			private void showRestingTrainsInRoute(List<DataStopTime> stopList, String routeName){
+				Vector rowData = new Vector();
+				for (DataStopTime stop : stopList) {
+					//String tripId = stop.trip_id;
+				      Vector colData = new Vector( Arrays.asList(stop.trip_id, this.stops.get(stop.stop_id).stop_name,stop.arrival_time,stop.departure_time,stop.stop_sequence.toString()));
+				      rowData.add(colData);
+			    }
+				
+				//Setup table
+			    String[] columnNames = {"Trip ID", "Stop Name", "Arrival Time", "Departure Time", "Stop Sequence"};
+			    
+			    Vector columnNamesV = new Vector(Arrays.asList(columnNames));
+			    final JTable table = new JTable(rowData, columnNamesV);
+			    
+			    JDialog fRestingInRoute = new JDialog();
+			    fRestingInRoute.setTitle("Current Route: "+ routeName);
+			    //f.setTitle("Departing Trains from " + this.stops.get(stop_id) );
+			    fRestingInRoute.setSize(600,600);
+			    fRestingInRoute.add(new JScrollPane(table));
+			    fRestingInRoute.setVisible(true);
+			    
+			    
+			    table.setRowSelectionAllowed(true);
+				
 			}
 	
 	
